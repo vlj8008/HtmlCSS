313ddf2e5fa2c277c14e5abacd907a1cf181e503
Vicky Jones <vlj8008@gmail.com>
2022-09-23 20:57:43 -0500

    Added pop-up to inform user using Vanilla JS
    Pop up appears after user clicks on search button informing user that results are coming up

---
+++ BootstrapLowCarbWS/main.js
@@ -0,0 +1,11 @@
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
+
+var submitButton = document.querySelector("#submit-btn");
+
+function submitMe()
+{
+    alert("You submitted a search. Results coming up...");
+}
+
