a1343c5070f5c36840438a154f5652611210de98
Vicky Jones <vlj8008@gmail.com>
2022-09-23 21:29:17 -0500

    Titles now change font size and color when hovered over.
    When titles of various recipes are hovered over they now change color and font size (become bigger)

--- BootstrapLowCarbWS/main.js
+++ BootstrapLowCarbWS/main.js
@@ -9,3 +9,24 @@ function submitMe()
     alert("You submitted a search. Results coming up...");
 }
 
+
+
+function changeFontColor()
+{
+    document.getElementById("title1").style.color = "darkblue";
+    document.getElementById("title2").style.color = "darkblue";
+    document.getElementById("title3").style.color = "darkblue";
+}
+
+function changeFontSize()
+{
+    document.getElementById("title1").style.fontSize = "24px";
+    document.getElementById("title2").style.fontSize = "24px";
+    document.getElementById("title3").style.fontSize = "24px";
+}
+
+function changeColorFontSize()
+{
+    changeFontColor();
+    changeFontSize();
+}
